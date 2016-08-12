48de12c240a357aeeb7e017d3bfe5377727daf47
Alexander Vostriakov <alexander.vostriakov@epfl.ch>
2016-08-12 11:40:18 +0200

    RESTful API routes

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// set up ======================================================
+// set up ======================================================================
 var express         = require('express');
 var app             = express();
 var mangoose        = require('mongoose');
@@ -6,7 +6,7 @@ var morgan          = require('morgan');
 var bodyParser      = require('body-parser');
 var methodOverride  = require('method-override');
 
-// configuration ===============================================
+// configuration ===============================================================
 mangoose.connect('mongodb://localhost:27017/node-todo');
 
 app.use(express.static(__dirname + '/public'));
@@ -16,11 +16,62 @@ app.use(bodyParser.json());
 app.use(bodyParser.json({ type: 'application/vnd.api+json'}));
 app.use(methodOverride());
 
-// define model ================================================
+// define model ================================================================
 var Todo = mongoose('Todo', {
   text : String
 })
 
-// listen (start app with node server.js) ======================
+// listen (start app with node server.js) ======================================
 app.listen(8080);
 console.log("App listening on port 8080");
+
+// routes ======================================================================
+
+  //api ------------------------------------------------------------------------
+  // get all todos
+  app.get('/api/todos', function(req, res){
+    // use mangoose to get all todos in the database
+    Todo.find(function(err, todos){
+      // if there is an error retrieving, send the error. Nothing after res.send
+      if (err)
+        res.send(err);
+
+      res.json(todos); // return all todos in JSON format
+    });
+  });
+
+  // create todo and send back all todos after creation
+  app.post('/api/todos', function(req,res){
+    // create a todo, information comes from AJAX request from Angular
+    Todo.create({
+      text : req.body.text,
+      done : false
+    }, function(err, todo){
+      if (err)
+        res.send(err);
+
+      // get and return all the todos after you create another
+      Todo.find(function(err, todos){
+        if (err)
+          res.send(err);
+        res.json(todos);
+      });
+    });
+  });
+
+  // delete a todo
+  app.delete('/api/todos/:todo_id', function(req,res) {
+    Todo.remove({
+      _id : req.params.todo_id
+    }, function(err, todo) {
+      if (err)
+        res.send(err);
+
+      // get and return all the todos after you create another
+      Todo.find(function(err, todos) {
+        if (err)
+          res.send(err);
+        res.json(todos);
+      });
+    });
+  });
